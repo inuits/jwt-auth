b1549e7da7b16922e600f95d3bb7dfbe278a2f6c
Bram Oosterlynck <booster@inuits.eu>
2022-08-11 14:50:26 +0200

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="inuits_jwt_auth",
-    version="0.0.31",
+    version="0.0.32",
     description="A Wrapper for authlib library with roles and permissions",
     classifiers=[
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
